40d5da2863eac99396a819535a2144eb6d4211c9
Sopar Sihotang <soparsihotang@gmail.com>
2017-10-18 01:39:26 +0000

    Add class service provider

---
+++ src/GG/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GG;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
